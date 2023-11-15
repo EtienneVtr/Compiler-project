4a215a9dc8eaf3ccb8c797e06fa90e476a2acf49
Matthias CABILLOT <matthias.cabillot@telecomnancy.eu>
2023-11-15 09:16:55 +0100

    Affiche l'AST

--- sample/AST.py
+++ sample/AST.py
@@ -11,11 +11,18 @@ class Node:
         return len(self.children)
     def rm_children(self) -> 'Node':
         return self.children.pop()
+    def to_str(self) -> str:
+        rtn = f"{self.type}: {', '.join([child.type for child in self.children])}"
+        for child in self.children:
+            if child.nbr_children():
+                rtn += "\n" + child.to_str()
+        return rtn
     def __str__(self) -> str:
+        return self.to_str()
+    def __repr__(self) -> str:
         if self.nbr_children() == 0:
             return self.type
-        else:
-            return f"{self.type}({', '.join([child.__str__() for child in self.children])})"
+        return f"{self.type}({', '.join([child.__repr__() for child in self.children])})"
 class AST:
     def __init__(self):
         self.root = Node("FICHIER")
@@ -23,7 +30,8 @@ class AST:
         return self.root.__str__()
 
 
+
 if __name__=="__main__":
     ast = AST()
     ast.root.add_child(Node("a").add_child(Node("b")).add_child(Node("c")))
-    print(ast.root)
+    print(ast)
