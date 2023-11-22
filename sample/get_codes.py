e57f0f26698478da351b86cf7c3c0a686a96b515
Matthias CABILLOT <matthias.cabillot@telecomnancy.eu>
2023-11-22 13:56:34 +0100

    Improve get_code.py

--- sample/get_codes.py
+++ sample/get_codes.py
@@ -1,11 +1,17 @@
-from token_pcl import codes
+from token_pcl import codes, get_keyword
 
 print(codes)
+
+print("\nEnter a code or keyword to get the other one.\nCtrl+C to exit.\n")
 
 while True:
     try:
         code = input(">> ")
-        print(codes.get(code, "Not found"))
+        if code.isdigit():
+            code = int(code)
+            print(get_keyword(code))
+        else:
+            print(codes.get(code, "Not found"))
     except KeyboardInterrupt:
         print()
         exit(0)
